27f80d5c8ec02f13c94df810a7a30a7869bf8152
Philipp <pd95@users.noreply.github.com>
2022-01-31 17:05:00 +0100

    Initial Commit

---
+++ WhatsNext/WhatsNextApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsNextApp.swift
+//  WhatsNext
+//
+//  Created by Philipp on 31.01.22.
+//
+
+import SwiftUI
+
+@main
+struct WhatsNextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
